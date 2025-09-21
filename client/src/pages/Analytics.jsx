78f58b53378de9f706a4a6c6b823e367700a5545
Rohit-998 <rohitagrawal99819@gmail.com>
2025-09-21 14:35:16 +0530

    fix

--- client/src/pages/Analytics.jsx
+++ client/src/pages/Analytics.jsx
@@ -25,7 +25,7 @@ export default function Analytics() {
   if (!analytics) return <p className="text-center">No analytics data available.</p>;
 
   return (
-    <div className=" bg-gray-800/50 p-8 rounded-lg shadow-lg">
+    <div className=" bg-gray-800/60 p-8 rounded-lg shadow-lg">
       <h1 className="text-3xl font-bold mb-6">Library Analytics</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-center">
         <div className="bg-gray-700 p-4 rounded-lg">
