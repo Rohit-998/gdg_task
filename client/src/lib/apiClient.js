f7749fba5c70211c1efa62342a6c2010abef77ff
Rohit-998 <rohitagrawal99819@gmail.com>
2025-09-21 14:11:59 +0530

    fix

--- client/src/lib/apiClient.js
+++ client/src/lib/apiClient.js
@@ -5,18 +5,18 @@ const API = axios.create({
   withCredentials: true,
 });
 
-export const signup = (data) => API.post("/api/auth/signup", data);
-export const signin = (data) => API.post("/api/auth/login", data);
-export const logout = () => API.post("/api/auth/logout");
+export const signup = (data) => API.post("/api/auth/signup", data,{withCredentials:true});
+export const signin = (data) => API.post("/api/auth/login", data,{withCredentials:true});
+export const logout = () => API.post("/api/auth/logout",{withCredentials:true});
 
 export const getBooks = (params) => API.get("/api/books", { params });
-export const addBook = (data) => API.post("/api/books/addbook", data);
-export const deleteBook = (id) => API.delete(`/api/books/deletebook/${id}`);
-export const updateBook = (id, data) => API.put(`/api/books/updatebook/${id}`, data);
-export const borrowBook = (id) => API.post(`/api/books/borrow/${id}`);
-export const returnBook = (id) => API.post(`/api/books/return/${id}`);
+export const addBook = (data) => API.post("/api/books/addbook", data,{withCredentials:true});
+export const deleteBook = (id) => API.delete(`/api/books/deletebook/${id}`,{withCredentials:true});
+export const updateBook = (id, data) => API.put(`/api/books/updatebook/${id}`, data,{withCredentials:true});
+export const borrowBook = (id) => API.post(`/api/books/borrow/${id}`,{withCredentials:true});
+export const returnBook = (id) => API.post(`/api/books/return/${id}`,{withCredentials:true});
 
-export const getUserDetails = () => API.get("/api/users/user");
-export const getDashboardBooks = () => API.get("/api/users/dashboard");
+export const getUserDetails = () => API.get("/api/users/user",{withCredentials:true});
+export const getDashboardBooks = () => API.get("/api/users/dashboard",{withCredentials:true});
 
-export const getAnalytics = () => API.get("/api/books/analytics");
+export const getAnalytics = () => API.get("/api/books/analytics",{withCredentials:true});
